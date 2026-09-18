4ad730535d652c69e5257f505711312968acb330
Therealmaffolone <andre.maffo@gmail.com>
2026-09-19 00:38:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='maffolone-action-v03';
+const CACHE='maffolone-overlay-v04';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./sw.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
